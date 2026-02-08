91a9c198aa2cce4558230e8c8036559fd0f8d5e7
Nix <204848365+Nixu-dev@users.noreply.github.com>
2026-02-08 00:39:52 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: 'https://nixu-dev.github.io/blog-vue/',
+  base: './blog-vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
